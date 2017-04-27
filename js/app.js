6c98dbaacf90697669fdb2df49e2b6880b7659c0
石村 彰大 <fr622821@FRI.FUJITSU.COM>
2017-04-27 15:25:45 +0900

    try to add line chart

--- js/app.js
+++ js/app.js
@@ -34,7 +34,6 @@ function hmapGenerator(fp){
 				 }
 	  });
 };
-
 var map1 = L.map('map1', {
 		center: [35.52886542,139.2724587],
 		zoom: 18,
@@ -50,6 +49,118 @@ var overlayMaps = {
 		"K": MapK,
 };
 L.control.layers(baseMaps, overlayMaps).addTo(map1);
+add_markers();
+
+//Generate Pins
+function add_markers(){
+		var points = [["Sens01", 35.52886542,139.2724587, "east"],
+									["Sens02", 35.42886542,139.1724587, "center"],
+									["Sens03", 35.62886542,139.3724587, "west"]
+		];
+		var marker = [];
+		var i;
+		for(i = 0; i < points.length; i++){
+			marker[i] = new L.Marker([points[i][1], points[i][2]],{
+				sens_no: points[i][3]
+			});
+			marker[i].addTo(map1);
+			marker[i].on('click', onClick);
+		};
+}
+
+// Evnet Handler
+function onClick(e){
+	console.log(this.options.sens_no);
+	var val_moi = ["Moist10cm", "Moist40cm", "Moist70cm"],
+			val_tmp = ["Temp10cm", "Temp40cm", "Temp70cm"];
+	var fn = "./js/decagon_real.csv";
+	var sens_no = this.options.sens_no;
+	draw_chart(val_moi, fn, sens_no, idd = "#graphZone");
+	draw_chart(val_tmp, fn, sens_no, idd = "#graphZone2");
+}
+
+// Draw Line Chart
+// Copying https://bl.ocks.org/d3noob/4db972df5d7efc7d611255d1cc6f3c4f
+function draw_chart(val, fn, sens_no, idd){
+	// set the dimensions and margins of the graph
+	var margin = {top: 20, right: 20, bottom: 30, left: 50},
+			width = 470 - margin.left - margin.right,
+			height = width * .5 - margin.top - margin.bottom;
+
+	// parse the date / time
+	var parseTime = d3.timeParse("%Y/%m/%d %H:%M");
+
+	// set the ranges
+	var x = d3.scaleTime().range([0, width]);
+	var y = d3.scaleLinear().range([height, 0]);
+	console.log(val[0]);
+	// define the 1st line
+	var valueline = d3.line()
+		.x(function(d){return x(d.Time); })
+		.y(function(d){return y(d[val[0]]); });
+	// define the 2nd line
+	var valueline2 = d3.line()
+		.x(function(d){return x(d.Time); })
+		.y(function(d){return y(d[val[1]]); });
+	// defien the 3rd line
+	var valueline3 = d3.line()
+		.x(function(d){return x(d.Time); })
+		.y(function(d){return y(d[val[2]]); });
+
+	// append the svg object to the graphZone of the page
+	// append a 'group' element to 'svg'
+	// moves the 'group' element to the top left margin
+	var svg = d3.select(idd).append("svg")
+				.attr("width", width + margin.top + margin.right)
+				.attr("height", height + margin.top + margin.bottom)
+			.append("g")
+				.attr("transform",
+								"translate(" + margin.left + "," + margin.top + ")");
+	// Get the data
+	d3.csv(fn, function(error, data){
+		if(error) throw error;
+
+		// format the data
+		data.forEach(function(d){
+			d.Time = parseTime(d.Time);
+			d[val[0]] = +d[val[0]];
+			d[val[1]] = +d[val[1]];
+			d[val[2]] = +d[val[2]];
+		});
+
+		// Scale the range of the data
+		x.domain(d3.extent(data, function(d){return d.Time; }));
+		y.domain([d3.min(data, function(d){
+								return Math.min(d[val[0]], d[val[1]], d[val[2]]);}),
+							d3.max(data, function(d){
+								return Math.max(d[val[0]], d[val[1]], d[val[3]]);})]);
+		// Add the valueline path
+		svg.append("path")
+			.data([data])
+			.attr("class", "line")
+			.style("stroke", "blue")
+			.attr("d", valueline);
+		// Add the valueline2 path
+		svg.append("path")
+			.data([data])
+			.attr("class", "line")
+			.style("stroke", "red")
+			.attr("d", valueline2);
+		// Add the valueline2 path
+		svg.append("path")
+			.data([data])
+			.attr("class", "line")
+			.style("stroke", "green")
+			.attr("d", valueline3);
+
+		// Add the X axis
+		svg.append("g")
+			.attr("transform", "translate(0," + height + ")")
+			.call(d3.axisBottom(x));
+		// Add the Y axis
+		svg.append("g").call(d3.axisLeft(y));
+	});
+}
 
 // Read CSV and Convert to array
 function csv2Array(filePath) {
