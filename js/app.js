9ff0052f378728c7e711ac6c5222aebdeca1db40
石村 彰大 <fr622821@FRI.FUJITSU.COM>
2017-04-27 10:01:23 +0900

    add heatmap

--- js/app.js
+++ js/app.js
@@ -1,50 +1,71 @@
 // Require leaflet.heat plugin
 // http://day-journal.com/blog/leaflet-027-%E3%83%92%E3%83%BC%E3%83%88%E3%83%9E%E3%83%83%E3%83%97/
+
 var mapTiles = L.tileLayer(
 	'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
 	{
 		attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a>',
-		maxZoom: 18
+		maxZoom: 22
 	});
+// mapboxgl.accessToken = 'pk.eyJ1IjoicGVybm9kMTQiLCJhIjoiY2owdnU2bGJnMDA0ZTJ3cWk0c2pwZmZuciJ9.JzziXsub9lijkthfcCO6eQ';
+// var map = new mapboxgl.Map({
+// 	center: [139.2724587, 35.52886542],
+// 	zoom: 18,
+// 	container: 'map',
+// 	style: 'mapbox://styles/mapbox/dark-v9'
+// });
 
-var map = L.map('map', {
-	center: [35.8529, 139.4122],
-	zoom: 14,
-	layers: [mapTiles],
+// Set filePath and col Number
+var MapCL = hmapGenerator("./js/dataCL.csv");
+var MapDD = hmapGenerator("./js/dataDD.csv");
+var MapK = hmapGenerator("./js/dataK.csv");
+
+// Generator of Heatmaap
+function hmapGenerator(fp){
+	return L.heatLayer(csv2Array(fp),
+			{
+			radius : 12,
+			// color of heatmap
+			gradient: {
+						 0.7: 'blue',
+						 0.8: 'green',
+						 0.9: 'yellow',
+						 1.0: 'red'
+				 }
+	  });
+};
+
+var map1 = L.map('map1', {
+		center: [35.52886542,139.2724587],
+		zoom: 18,
+		layers: [mapTiles, MapCL, MapDD, MapK],
 });
 
-var heat = L.heatLayer([
-    [35.8559, 139.4122, 20],
-    [35.8529, 139.4121, 20],
-    [35.8529, 139.4120, 10],
-    [35.8528, 139.4122, 50],
-    [35.8528, 139.4121, 40],
-    [35.8528, 139.4120, 10],
-    [35.8517, 139.4122, 10],
-    [35.8517, 139.4121, 10],
-    [35.8507, 139.4120, 10],
-  ],{
-    radius: 10
-  }).addTo(map);
-
+var baseMaps = {
+    "baseMap": mapTiles
+};
+var overlayMaps = {
+    "CL": MapCL,
+		"DD": MapDD,
+		"K": MapK,
+};
+L.control.layers(baseMaps, overlayMaps).addTo(map1);
 
 // Read CSV and Convert to array
-function getCSV(){
-    var req = new XMLHttpRequest();
-    req.open("GET", "./js/sample.csv", true);
-    //req.send(null);
+function csv2Array(filePath) {
+	var csvData = new Array();
+	var req = new XMLHttpRequest();
+	req.open("GET", filePath, false);
+	req.send(null);
 
-    req.onload = function(){
-	     convertCSVtoArray(req.responseText);
-    }
-}
-
-function convertCSVtoArray(str){
-    var result = [];
-    var tmp = str.split("\n");
-
-    for(var i=0;i<tmp.length;++i){
-        result[i] = tmp[i].split(',');
-    }
-    console.log(result[1][2]); // 300yen
+	var lines = req.responseText.split("\n");
+	for (var i = 0; i < lines.length; ++i) {
+		var cells = lines[i].split(",");
+		if( cells.length != 1 ) {
+			// Cast as Number
+			cells = cells.map(function(element){return parseFloat(element);});
+			csvData.push(cells);
+		}
+	}
+	return csvData;
 }
